c163e8407c33547658e0f098eb69d6c090467b41
Rovasoa <andrianiainarovasoaherimandimb@gmail.com>
2025-07-19 13:08:32 +0200

    init/geolocalisation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Map from "./components/mapWrapper/map.jsx";
+function App() {
+  return (
+    <>
+      <h1>Bienvenue à bord, explorateur !</h1>
+      <p>Voici ta carte interactive, prête à te suivre partout.</p>
+      <Map />
+    </>
+  );
+}
+
+export default App;
